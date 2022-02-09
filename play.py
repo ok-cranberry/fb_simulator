f3b94ff4e3dccaf740f3b52021087fbe723ae63b
stoth <junksteve9145@gmail.com>
2022-02-09 09:24:27 -0500

    created position classes with basic attributes

--- play.py
+++ play.py
@@ -1,12 +1,41 @@
+import random
+
+class Quarterback:
+    def __init__(self, name):
+        self.name = name
+        setattr(self, "accuracy", random.randrange(1,20))
+
+class WideReciever:
+    def __init__(self, name):
+        self.name = name
+        setattr(self, "catching", random.randrange(1,20))
+
+class DefensiveBack:
+    def __init__(self, name):
+        self.name = name
+        setattr(self, "coverage", random.randrange(1,20))
+
+
+
 def presnap():
     pass
 
 def action():
     pass
+
 def result():
     pass
 
 def main():
+
+    QB = Quarterback("Charlie Manack")
+    WR = WideReciever("Hunter Patterson")
+    DB = DefensiveBack("Chris Pierce")
+
+    print(QB.accuracy)
+    print(WR.catching)
+    print(DB.coverage)
+
     presnap()
     action()
     result()
