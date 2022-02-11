949229eaa1d8f81e1d22fe5a29f1e1bf20139f1a
stoth <junksteve9145@gmail.com>
2022-02-11 17:14:40 -0500

    added a player database to distinguish teams

---
+++ player_database.py
@@ -0,0 +1,4 @@
+db = {
+    "Frazier": {"QB": "Charlie Manack", "WR": "Hunter Patterson", "DB": "Chris Pierce"},
+    "Geibel": {"QB": "Tyler Erdely", "WR": "Mark Erdely", "DB": "Walter Erdely"},
+}
--- team.py
+++ team.py
@@ -1,10 +1,11 @@
 import positions
+from player_database import db
 
 
 class Team:
     def __init__(self, name):
         self.name = name
 
-        self.quarterback = positions.Quarterback("Charlie Manack")
-        self.wide_receiver = positions.WideReceiver("Hunter Patterson")
-        self.defensive_back = positions.DefensiveBack("Chris Pierce")
+        self.quarterback = positions.Quarterback(db[self.name]["QB"])
+        self.wide_receiver = positions.WideReceiver(db[self.name]["WR"])
+        self.defensive_back = positions.DefensiveBack(db[self.name]["DB"])
