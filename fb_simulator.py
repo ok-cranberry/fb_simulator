6ffa9e8aeac93ad543c12ec44317440b09792ccd
stoth <junksteve9145@gmail.com>
2022-03-03 14:31:35 -0500

    modified game loop, and trialed a mouse over visual change

--- fb_simulator.py
+++ fb_simulator.py
@@ -2,8 +2,39 @@ from team import Team
 from league import League
 import argparse
 import pygame
-from pygame.locals import *
-import sys
+from pygame.locals import K_ESCAPE, KEYDOWN, QUIT
+
+
+pygame.init()
+
+SCREEN_WIDTH = 500
+SCREEN_HEIGHT = 500
+FONT = pygame.font.SysFont("Arial", 20)
+FONT_COLOR = pygame.Color('black')
+
+
+class PressText(pygame.sprite.Sprite):
+    def __init__(self, element):
+        super(PressText, self).__init__()
+        self.surf = pygame.Surface((200, 50))
+        self.surf.fill((255, 255, 255))
+        self.text_surf = FONT.render(element, True, FONT_COLOR)
+        self.text_rect = self.text_surf.get_rect(
+            center=self.surf.get_rect().center
+        )
+        self.surf.blit(self.text_surf, self.text_rect)
+
+        self.rect = self.surf.get_rect()
+
+    def update(self):
+        mouse_pos = pygame.mouse.get_pos()
+        if self.rect.collidepoint(mouse_pos):
+            self.surf.fill((0, 255, 0))
+            self.surf.blit(self.text_surf, self.text_rect)
+
+        if not self.rect.collidepoint(mouse_pos):
+            self.surf.fill((255, 255, 255))
+            self.surf.blit(self.text_surf, self.text_rect)
 
 
 def main():
@@ -32,16 +63,46 @@ def main():
         conference.season_stats()
 
     else:
-        pygame.init()
-        DISPLAYSURF = pygame.display.set_mode((400, 300))
-        pygame.display.set_caption('Football Simulator 2022')
 
-        while True:
+        screen = pygame.display.set_mode([SCREEN_WIDTH, SCREEN_HEIGHT])
+
+        box = PressText("Player Name")
+
+        all_sprites = pygame.sprite.Group()
+        all_sprites.add(box)
+
+        game_running = True
+
+        while game_running:
             for event in pygame.event.get():
-                if event.type == QUIT:
-                    pygame.quit()
-                    sys.exit()
-            pygame.display.update()
+                if event.type == KEYDOWN:
+                    if event.key == K_ESCAPE:
+                        # eventually replace to back out of a menu or prompt
+                        game_running = False
+                elif event.type == pygame.QUIT:
+                    game_running = False
+
+                pressed_keys = pygame.key.get_pressed()
+
+                # update state of game objects
+                for sprite in all_sprites:
+                    sprite.update()
+
+                # Draw stuff here
+
+                surf_center = (
+                    (SCREEN_WIDTH - box.surf.get_width()) / 2,
+                    (SCREEN_HEIGHT - box.surf.get_height()) / 2,
+                )
+
+                for entity in all_sprites:
+                    screen.blit(entity.surf, entity.rect)
+
+                pygame.display.flip()
+
+                # maintain game speed
+
+        pygame.quit()
 
 
 if __name__ == "__main__":
--- team.py
+++ team.py
@@ -16,5 +16,13 @@ class Team:
             self.quarterback = positions.Quarterback("QB #10")
             self.running_back = positions.RunningBack("RB #22")
             self.wide_receiver = positions.WideReceiver("WR #81")
-            self.linebacker = positions.Linebacker("LB # 55")
-            self.defensive_back = positions.DefensiveBack("DB # 25")
+            self.linebacker = positions.Linebacker("LB #55")
+            self.defensive_back = positions.DefensiveBack("DB #25")
+
+        self.roster = {
+            "QB": self.quarterback,
+            "RB": self.running_back,
+            "WR": self.wide_receiver,
+            "LB": self.linebacker,
+            "DB": self.defensive_back,
+        }
