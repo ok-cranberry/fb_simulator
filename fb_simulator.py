241b62a8fadf31cccb359388bccbc80e65b91b19
stoth <junksteve9145@gmail.com>
2022-04-10 17:51:37 -0400

    Created State Machine, corrected button logic

--- fb_simulator.py
+++ fb_simulator.py
@@ -1,40 +1,172 @@
 from team import Team
 from league import League
 import argparse
+import time
+from enum import Enum
 import pygame
 from pygame.locals import K_ESCAPE, KEYDOWN, QUIT
 
 
-pygame.init()
+class MenuButton(pygame.sprite.Sprite):
+    def __init__(self, msg: str, position: tuple, fb_simulator):
+        super(MenuButton, self).__init__()
+        self.x = position[0]
+        self.y = position[1]
+        self.width = 200
+        self.height = 50
+        self.fill_color = (255, 255, 255)
+        self.msg = msg
+        self.position = position
 
-SCREEN_WIDTH = 500
-SCREEN_HEIGHT = 500
-FONT = pygame.font.SysFont("Arial", 20)
-FONT_COLOR = pygame.Color('black')
+        self.surf = pygame.Surface((self.width, self.height))
+        self.rect = pygame.Rect(self.x, self.y, self.width, self.height)
 
+        self.fb_simulator = fb_simulator
 
-class PressText(pygame.sprite.Sprite):
-    def __init__(self, element):
-        super(PressText, self).__init__()
-        self.surf = pygame.Surface((200, 50))
-        self.surf.fill((255, 255, 255))
-        self.text_surf = FONT.render(element, True, FONT_COLOR)
-        self.text_rect = self.text_surf.get_rect(
-            center=self.surf.get_rect().center
+    def mouse_is_over(self):
+        mouse_pos = pygame.mouse.get_pos()
+
+        if self.rect.collidepoint(mouse_pos):
+            return True
+        else:
+            return False
+
+    def draw(self, window):
+        self.text = self.fb_simulator.FONT.render(
+            self.msg, True, self.fb_simulator.FONT_COLOR
         )
-        self.surf.blit(self.text_surf, self.text_rect)
+        center = (
+            (self.width / 2 - self.text.get_width() / 2),
+            (self.height / 2 - self.text.get_height() / 2),
+        )
 
-        self.rect = self.surf.get_rect()
+        self.surf.fill(self.fill_color)
+
+        if self.mouse_is_over():
+            self.surf.fill((0, 255, 0))
+
+        self.surf.blit(self.text, center)
+        window.blit(self.surf, self.position)
+
+    def click(self):
+        # Probably a future state change action
+        # This is a WIP
+        if self.fb_simulator.actions["mouse_click"]:
+            if self.mouse_is_over():
+                print("button click")
+                return True
+        else:
+            return False
+
+
+class GameState:
+    def __init__(self, fb_simulator):
+        self.fb_simulator = fb_simulator
+        self.prev_state = None
+
+    def update(self, delta_time, actions):
+        pass
+
+    def render(self, window):
+        pass
+
+    def enter_state(self):
+        if len(self.fb_simulator.state_stack) > 1:
+            self.prev_state = self.fb_simulator.state_stack[-1]
+        self.fb_simulator.state_stack.append(self)
+
+    def exit_state(self):
+        self.fb_simulator.state_stack.pop()
+
+
+class MainMenuState(GameState):
+    def __init__(self, fb_simulator):
+        GameState.__init__(self, fb_simulator)
+
+        self.start_button = MenuButton("Start!", (100, 300), self.fb_simulator)
+        self.quit_button = MenuButton("Quit", (100, 375), self.fb_simulator)
+
+    def update(self, delta_time):
+
+        if self.start_button.click():
+            new_state = TeamSelectionState(self.fb_simulator)
+            new_state.enter_state()
+        if self.quit_button.click():
+            self.fb_simulator.game_running = False
+
+    def render(self, window):
+        self.start_button.draw(window)
+        self.quit_button.draw(window)
+
+
+class TeamSelectionState(GameState):
+    def __init__(self, fb_simulator):
+        GameState.__init__(self, fb_simulator)
+
+    def update(self, delta_time):
+        pass
+
+    def render(self, window):
+        window.fill((0, 0, 0))
+
+
+class FootballSimulator:
+    def __init__(self):
+        pygame.init()
+
+        self.SCREEN_WIDTH = 500
+        self.SCREEN_HEIGHT = 500
+        self.FONT = pygame.font.SysFont("Arial", 20)
+        self.FONT_COLOR = pygame.Color('red')
+        self.screen = pygame.display.set_mode(
+            [self.SCREEN_WIDTH, self.SCREEN_HEIGHT]
+        )
+        self.dt, self.prev_time = 0, 0
+        self.actions = {"mouse_click": False}
+
+        self.game_running = True
+
+        self.state_stack = []
+        self.load_states()
+
+    def game_loop(self):
+        while self.game_running:
+            self.get_dt()
+            self.get_events()
+            self.update()
+            self.render()
+            print(self.actions['mouse_click'])
+
+    def get_events(self):
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                self.game_running = False
+            elif event.type == KEYDOWN:
+                if event.key == K_ESCAPE:
+                    # eventually replace to back out of a menu or prompt
+                    self.game_running = False
+            elif event.type == pygame.MOUSEBUTTONDOWN:
+                if event.button == 1:
+                    self.actions['mouse_click'] = True
+            elif event.type == pygame.MOUSEBUTTONUP:
+                if event.button == 1:
+                    self.actions['mouse_click'] = False
 
     def update(self):
-        mouse_pos = pygame.mouse.get_pos()
-        if self.rect.collidepoint(mouse_pos):
-            self.surf.fill((0, 255, 0))
-            self.surf.blit(self.text_surf, self.text_rect)
+        self.state_stack[-1].update(self.dt)
 
-        if not self.rect.collidepoint(mouse_pos):
-            self.surf.fill((255, 255, 255))
-            self.surf.blit(self.text_surf, self.text_rect)
+    def render(self):
+        self.state_stack[-1].render(self.screen)
+        pygame.display.flip()
+
+    def get_dt(self):
+        now = time.time()
+        self.dt = now - self.prev_time
+        self.prev_time = now
+
+    def load_states(self):
+        self.main_menu = MainMenuState(self)
+        self.state_stack.append(self.main_menu)
 
 
 def main():
@@ -63,44 +195,9 @@ def main():
         conference.season_stats()
 
     else:
-
-        screen = pygame.display.set_mode([SCREEN_WIDTH, SCREEN_HEIGHT])
-
-        box = PressText("Player Name")
-
-        all_sprites = pygame.sprite.Group()
-        all_sprites.add(box)
-
-        game_running = True
-
-        while game_running:
-            for event in pygame.event.get():
-                if event.type == KEYDOWN:
-                    if event.key == K_ESCAPE:
-                        # eventually replace to back out of a menu or prompt
-                        game_running = False
-                elif event.type == pygame.QUIT:
-                    game_running = False
-
-                pressed_keys = pygame.key.get_pressed()
-
-                # update state of game objects
-                for sprite in all_sprites:
-                    sprite.update()
-
-                # Draw stuff here
-
-                surf_center = (
-                    (SCREEN_WIDTH - box.surf.get_width()) / 2,
-                    (SCREEN_HEIGHT - box.surf.get_height()) / 2,
-                )
-
-                for entity in all_sprites:
-                    screen.blit(entity.surf, entity.rect)
-
-                pygame.display.flip()
-
-                # maintain game speed
+        fb = FootballSimulator()
+        while fb.game_running:
+            fb.game_loop()
 
         pygame.quit()
 
